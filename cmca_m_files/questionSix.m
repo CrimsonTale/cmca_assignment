69f3681298dc941461de70d05f7ecf52a0511022
SimplyNesh <neshnotion@gmail.com>
2020-08-06 12:21:17 +0800

    added question 6 .m file

--- cmca_m_files/questionSix.m
+++ cmca_m_files/questionSix.m
@@ -1,135 +1,127 @@
 clc;
 clear;
+prompt = 'Would you like to carry on with purchase?[Y/N]: ';
+additionalPurchase = upper(input(prompt, 's'));
+while ~isempty(additionalPurchase) || not(strcmp(additionalPurchase, 'Y')) && not(strcmp(additionalPurchase, 'N'))
+    if strcmp(additionalPurchase, 'Y')
+  
+            prompt = 'Enter name: ';
+            userName = input(prompt, 's');
 
-prompt = 'Enter name: ';
-userName = input(prompt, 's');
+            while isempty(userName) || ~isnan(str2double(userName)) 
+                disp('Name cannot be blank or contain numbers...')
+                prompt = 'Enter name: ';
+                userName = input(prompt, 's');
+            end
 
-%{
-   For userPurchase, we must reject all isstrings and i,j to reject(cos i and j
-   are treated as doubles and accepted. We want to reject all letters.
-%}
-while isempty(userName) || ~isnan(str2double(userName)) 
-    disp('Name cannot be blank or contain numbers...')
-    prompt = 'Enter name: ';
-    userName = input(prompt, 's');
-end
+            prompt = 'Enter address: ';
+            userAddress = input(prompt, 's');
 
-prompt = 'Enter address: ';
-userAddress = input(prompt, 's');
+            while isempty(userAddress)
+                disp('Address cannot be blank...');
+                prompt = 'Enter address: ';
+                userAddress = input(prompt, 's');
+            end
 
-while isempty(userAddress)
-    disp('Address cannot be blank...');
-    prompt = 'Enter address: ';
-    userAddress = input(prompt, 's');
-end
+            prompt = 'Enter amount of purchase: ';
+            userPurchase = input(prompt);
 
-prompt = 'Enter amount of purchase: ';
-userPurchase = input(prompt);
+            %{
+            userPurchase = input(prompt); <--- no 's' flag, so all inputs are doubles
+            isnumeric('userPurchase') for inputs 'i' and 'j' are logical = 1(true)
+            therefore isreal has to be used in this scenario, where i and j are not
+            chars, but imaginary numbers when input(prompt) is used.
+            %}
 
-while isempty(userPurchase) || userPurchase < 0 || isstring(userPurchase) || ~isreal(userPurchase)
-    disp('Amount must be an interger and possess value of 0 or more dollars...');
-    prompt = 'Enter amount of purchase(integer): ';
-    userPurchase = input(prompt);
-end
+            while isempty(userPurchase) || userPurchase < 0 || isstring(userPurchase) || ~isreal(userPurchase)
+                disp('Amount must be an interger and possess value of 0 or more dollars...');
+                prompt = 'Enter amount of purchase(integer): ';
+                userPurchase = input(prompt);
+            end
 
-prompt = 'Enter type of purchase(L for Laptop or D for Desktop): ';
-userChoice = upper(input(prompt, 's'));
-discount = 0;
-netAmount = 0;
-
-%{
-while isempty(userChoice) || strcmp(userChoice, 'L' && userChoice ~= 'D')
-    disp('Invalid type of purchase')
-    prompt = 'Do you wish to continue purchase? (Y/N)';
-    toContinue = upper(input(prompt, 's'));
-    while toContinue == 'Y'
-        prompt = 'Enter type of purchase(L for Laptop or D for Desktop): ';
-        userChoice = upper(input(prompt, 's'));
-        if toContinue == 'N'
-            break
-        elseif userChoice == 'L' || userChoice == 'D'
-            continue
-        else
             prompt = 'Enter type of purchase(L for Laptop or D for Desktop): ';
             userChoice = upper(input(prompt, 's'));
-        end
+            discount = 0;
+            netAmount = 0;
+
+
+
+            while isempty(userChoice) || not(strcmp(userChoice, 'L')) && not(strcmp(userChoice, 'D'))
+                disp('Invalid type of purchase')
+                prompt = 'Do you wish to continue purchase? [Y/N]: ';
+                toContinue = upper(input(prompt, 's'));
+                if strcmp(toContinue, 'Y') 
+                    prompt = 'Enter type of purchase(L for Laptop or D for Desktop): ';
+                    userChoice = upper(input(prompt, 's'));
+                    discount = 0;
+                    netAmount = 0;
+                elseif strcmp(toContinue, 'N')
+                    disp('You have exit purchase screen, Goodbye')
+                    break
+                else
+                    disp('Please enter [Y/N] only...');
+                    continue
+                end
+            end
+
+
+            if userChoice == 'L'
+                switch true
+                    case (0 <= userPurchase) && (userPurchase <= 250)
+                        discount = (0/100) * userPurchase;
+                        netAmount = userPurchase - discount;
+                    case (251 <= userPurchase) && (userPurchase <= 570)
+                        discount = (5/100) * userPurchase;
+                        netAmount = userPurchase - discount;
+                    case (571 <= userPurchase) && (userPurchase <= 1000)
+                        discount = (7.5/100) * userPurchase;
+                        netAmount = userPurchase - discount;
+                    case userPurchase > 1000
+                        discount = (10/100) * userPurchase;
+                        netAmount = userPurchase - discount;
+                end
+            elseif userChoice == 'D'
+                switch true
+                    case (0 <= userPurchase) && (userPurchase <= 250)
+                        discount = (5/100) * userPurchase;
+                        netAmount = userPurchase - discount;
+                    case (251 <= userPurchase) && (userPurchase <= 570)
+                        discount = (7.6/100) * userPurchase;
+                        netAmount = userPurchase - discount;
+                    case (571 <= userPurchase) && (userPurchase <= 1000)
+                        discount = (10.0/100) * userPurchase;
+                        netAmount = userPurchase - discount;
+                    case userPurchase > 1000
+                        discount = (15.0/100) * userPurchase;
+                        netAmount = userPurchase - discount;
+                end
+            end
+
+            fprintf('Name: %s\n', userName);
+            fprintf('Address: %s\n', userAddress);
+            fprintf('Net amount: $%d\n', netAmount);
+            
+            prompt = 'Continue? [Y/N]: ';
+            toContinue = upper(input(prompt, 's'));
+            if strcmp(toContinue, 'N')
+                disp('Thanks and see you again')
+                break
+            elseif strcmp(toContinue, 'Y')
+                continue
+            else
+                disp('Please enter [Y/N] only...');
+                prompt = 'Continue? [Y/N]: ';
+                toContinue = upper(input(prompt, 's'));
+            end
+
+
+    elseif strcmp(additionalPurchase, 'N')
+             disp('Thanks and see you again')
+             break
+    else
+            disp('Please enter [Y/N] only...');
+            prompt = 'Would you like to carry on with purchase?[Y/N]: ';
+            additionalPurchase = upper(input(prompt, 's'));   
     end
+
 end
-%}
-if userChoice == 'L'
-    switch true
-        case (0 <= userPurchase) && (userPurchase <= 250)
-            discount = (0/100) * userPurchase;
-            netAmount = userPurchase - discount;
-        case (251 <= userPurchase) && (userPurchase <= 570)
-            discount = (5/100) * userPurchase;
-            netAmount = userPurchase - discount;
-        case (571 <= userPurchase) && (userPurchase <= 1000)
-            discount = (7.5/100) * userPurchase;
-            netAmount = userPurchase - discount;
-        case userPurchase > 1000
-            discount = (10/100) * userPurchase;
-            netAmount = userPurchase - discount;
-    end
-elseif userChoice == 'D'
-    switch true
-        case (0 <= userPurchase) && (userPurchase <= 250)
-            discount = (5/100) * userPurchase;
-            netAmount = userPurchase - discount;
-        case (251 <= userPurchase) && (userPurchase <= 570)
-            discount = (7.6/100) * userPurchase;
-            netAmount = userPurchase - discount;
-        case (571 <= userPurchase) && (userPurchase <= 1000)
-            discount = (10.0/100) * userPurchase;
-            netAmount = userPurchase - discount;
-        case userPurchase > 1000
-            discount = (15.0/100) * userPurchase;
-            netAmount = userPurchase - discount;
-    end
-end
-
-
-
-fprintf('Name: %s\n', userName);
-fprintf('Address: %s\n', userAddress);
-fprintf('Net amount: $%d\n', netAmount);
-
-%{
-    while ~isempty(toContinue)
-    toContinue = input('Do you want to continue purchase?, Y/N [Y]:','s');
-        if toContinue == 'N'
-            break
-        end
-    end
-
-%}
-
-
-%{
-    Sir, this is my reason for using ~isreal for userPurchase part.
-    A bit hard to explain just now because no display lol. Because by
-    right, we not supposed to make assumption that the user always know
-    what to input correct?
-
-
-   For userPurchase, we must reject all strings/letters including i and j 
-   (however, i and j are treated as numbers and accepted)
-    
-    // For this part(without isreal)... it rejects a-z except i and j.
-
-    prompt = 'Enter amount of purchase: ';
-    userPurchase = input(prompt);
-
-    while isempty(userPurchase) || userPurchase < 0 || isstring(userPurchase)
-        disp('Amount must be an interger and possess value of 0 or more dollars...');
-        prompt = 'Enter amount of purchase(integer): ';
-        userPurchase = input(prompt);
-    end
-
-    
-    so if user enters a-z, it will reject automatically but if he enters i
-    or j then the output will be accepted below, which we do not want.
-
-    >> userPurchase = 0.000000000000000 + 1.000000000000000i
-%}
-
